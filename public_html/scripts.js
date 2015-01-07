77456d75208e85eab4217a01eecceb5a0659f536
Joseph E. Roldan <omega6441@gmail.com>
2015-01-07 09:19:01 -0800

    beginning portfolio 3 with bits of bootstrap and program Jquery

---
+++ public_html/scripts.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$("document").ready(function() {
+    $(".wimp").css("background-color", "yellow");
+    
+    $('p:first').css("background-color","olive");
+});
+$("document").ready(function(){
+   $(".container").css("border","0");
+   $(".row").css("border","0");
+   $(".col-xs-2").css("border","0");
+    $(".col-xs-10").css("border","0");
+});
